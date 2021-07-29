8d5b338265a16ed0ee2c3952c2ede1653f2b3fc5
Renato Ribeiro <renato_ribeiro33@hotmail.com>
2021-07-28 21:12:00 -0300

    Page: Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,26 @@
-import Head from 'next/head'
+import Head from 'next/head';
+
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | Ignews</title>
+        <title>Home | Ignews</title>
       </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, Welcome</span>
+          <h1>New about the <span>React</span> world.</h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
     </>
   )
 }
